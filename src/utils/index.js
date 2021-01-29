6e4a4644ef390145f110b9c85e1df86e2a5743ac
Pace <handsome6491@outlook.com>
2021-01-28 17:05:00 -0700

    more currency

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,9 @@ import { toast } from 'react-toastify';
 //icons
 import { FaEthereum } from 'react-icons/fa';
 import { FaBtc } from 'react-icons/fa';
+import { SiLitecoin } from 'react-icons/si';
+import { SiBitcoin } from 'react-icons/si';
+import { SiXrp } from 'react-icons/si';
 
 export const toastr = {
   success: string => toast.success(`✔️ ${string}`),
@@ -13,5 +16,8 @@ export const toastr = {
 
 export const currencyIcons = [
   { symbol: 'ETH', icon: <FaEthereum /> },
-  { symbol: 'BTC', icon: <FaBtc /> },
+  { symbol: 'BTC', icon: <SiBitcoin /> },
+  { symbol: 'LTC', icon: <SiLitecoin /> },
+  { symbol: 'BCH', icon: <FaBtc /> },
+  { symbol: 'XRP', icon: <SiXrp /> },
 ];
